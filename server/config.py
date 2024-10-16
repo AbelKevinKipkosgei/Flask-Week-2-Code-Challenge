48e661f5e43bf04613d3db0ae97ae2c6da4965f5
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-16 11:03:16 +0300

    Configured and setup config.py.

--- server/config.py
+++ server/config.py
@@ -3,6 +3,7 @@
 
 from flask import Flask
 from flask_migrate import Migrate
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
@@ -19,4 +20,5 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+api = Api(app)
 
